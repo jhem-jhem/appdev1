009ae37309994107e958011fde73cd24506ca8b9
jhem-jhem <jhempr15@gmail.com>
2024-10-24 02:12:02 +0800

    I'm done completing the section using hooks

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,17 +1,7 @@
 import './App.css'
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 export default function MyApp() {
-  return (
-    <div>
-      <h1>Counters that update separately</h1>
-      <MyButton />
-      <MyButton />
-    </div>
-  );
-}
-
-function MyButton() {
   const [count, setCount] = useState(0);
 
   function handleClick() {
@@ -19,8 +9,20 @@ function MyButton() {
   }
 
   return (
-    <button onClick={handleClick}>
+    <div>
+      <h1>Counters that update together</h1>
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
+    </div>
+  );
+}
+
+function MyButton({ count, onClick }) {
+  return (
+    <button onClick={onClick}>
       Clicked {count} times
     </button>
   );
 }
+
+
