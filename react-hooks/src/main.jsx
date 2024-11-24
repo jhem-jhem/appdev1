7535b967bd0dfcae474296b9c694edb959365218
jhem-jhem <jhempr15@gmail.com>
2024-11-24 22:45:45 +0800

    Done in react hooks activity

---
+++ react-hooks/src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import Counter from './components/Counter.jsx'
+import ToggleButton from './components/ToggleButton.jsx'
+import FetchPosts from './components/FetchPosts.jsx'
+import TextInput from './components/TextInput.jsx'
+import { StudentProfile } from "./components/StudentProfile.jsx";
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    {/* <App /> */}
+    {/* <Counter /> */}
+    <ToggleButton />
+    {/* <FetchPosts /> */}
+    {/* <TextInput /> */}
+    {/* <StudentProfile /> */}
+  </StrictMode>,
+)
